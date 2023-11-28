67d98be071fd139649d315d452f83dc8225686be
stingotho <othoofficial@gmail.com>
2023-11-28 10:07:04 +0300

    snowflakes fix

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -26,16 +26,16 @@ const CountdownTimer = () => {
 
     useEffect(() => {
         // Snowflakes initialization
-        const newSnowflakes = Array.from({ length: 50 }, (_, id) => {
+        const initialSnowflakes = Array.from({ length: 50 }, (_, id) => {
             return {
                 id,
                 left: Math.random() * 100,
                 duration: 5 + Math.random() * 10,
-                delay: Math.random() * 5,
+                delay: -Math.random() * 5, // Negative delay for an immediate but staggered start
                 isVisible: true
             };
         });
-        setSnowflakes(newSnowflakes);
+        setSnowflakes(initialSnowflakes);
 
         // Countdown timer update
         const interval = setInterval(() => {
