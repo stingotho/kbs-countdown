6897c345162172ba1c16fe81975dec115832a673
stingotho <othoofficial@gmail.com>
2023-11-28 09:15:33 +0300

    snowflake and score is now displayed on the same screen instead of different components

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -2,40 +2,45 @@ import React, { useState, useEffect } from 'react';
 import Snowflake from './Snowflake';
 
 const CountdownTimer = () => {
-    const [timeLeft, setTimeLeft] = useState(/* ... initial time left state ... */);
+    const [timeLeft, setTimeLeft] = useState(/* ... */);
     const [score, setScore] = useState(0);
+    const [snowflakesVisibility, setSnowflakesVisibility] = useState(Array(50).fill(true));
+    const [snowflakes, setSnowflakes] = useState([]); // State for snowflakes
 
-    // ... existing countdown logic ...
+    useEffect(() => {
+        const newSnowflakes = createSnowflakes(50);
+        setSnowflakes(newSnowflakes);
+    }, []); // Only on initial render
 
-    const handleSnowflakeClick = () => {
+    const handleSnowflakeClick = (id) => {
         setScore(prevScore => prevScore + 1);
+        setSnowflakesVisibility(prevVisibility =>
+            prevVisibility.map((isVisible, index) => index === id ? false : isVisible));
     };
 
     const createSnowflakes = (num) => {
         return Array.from({ length: num }, (_, index) => {
             const left = Math.random() * 100;
             const animationDuration = 5 + Math.random() * 10;
-            const animationDelay = 0;
+            const animationDelay = Math.random() * 5;
 
             return (
-                <Snowflake 
-                    key={index} 
-                    id={index} 
-                    left={left} 
-                    duration={animationDuration} 
-                    delay={animationDelay} 
-                    onClick={handleSnowflakeClick} 
+                <Snowflake
+                    key={index}
+                    id={index}
+                    left={left}
+                    duration={animationDuration}
+                    delay={animationDelay}
+                    isVisible={snowflakesVisibility[index]}
+                    onClick={handleSnowflakeClick}
                 />
             );
         });
     };
 
-    const snowflakes = createSnowflakes(50);
-
     return (
         <div>
             <h1>❄️KBS Winter Break!❄️</h1>
-            {/* Countdown display */}
             <p>Click on the snowflakes to increase your score!</p>
             <div>Score: {score}</div>
             {snowflakes}
--- src/components/Snowflake.js
+++ src/components/Snowflake.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-const Snowflake = ({ id, left, duration, delay, onClick }) => {
+const Snowflake = ({ id, left, duration, delay, onClick, isVisible }) => {
     const style = {
         position: 'absolute',
         left: `${left}%`,
         top: '-5%', // Start above the viewport
         animationDuration: `${duration}s`,
         animationDelay: `${delay}s`,
-        cursor: 'pointer' // Change cursor to indicate clickable
+        cursor: 'pointer', // Change cursor to indicate clickable
+        display: isVisible ? 'block' : 'none', // Control visibility
     };
 
-    return <div className="snowflake" id={`snowflake-${id}`} style={style} onClick={() => onClick()}>❄️</div>;
+    return <div className="snowflake" style={style} onClick={() => onClick(id)}>❄️</div>;
 };
 
 export default Snowflake;
