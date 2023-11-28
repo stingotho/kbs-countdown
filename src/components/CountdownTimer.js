99121531803a36661cd6700d43618574380183a6
stingotho <othoofficial@gmail.com>
2023-11-28 09:17:54 +0300

    snowflakes are now disappearing after clicking on them

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -4,37 +4,28 @@ import Snowflake from './Snowflake';
 const CountdownTimer = () => {
     const [timeLeft, setTimeLeft] = useState(/* ... */);
     const [score, setScore] = useState(0);
-    const [snowflakesVisibility, setSnowflakesVisibility] = useState(Array(50).fill(true));
     const [snowflakes, setSnowflakes] = useState([]); // State for snowflakes
 
     useEffect(() => {
-        const newSnowflakes = createSnowflakes(50);
-        setSnowflakes(newSnowflakes);
-    }, []); // Only on initial render
+        setSnowflakes(createSnowflakes(50));
+    }, []);
 
     const handleSnowflakeClick = (id) => {
         setScore(prevScore => prevScore + 1);
-        setSnowflakesVisibility(prevVisibility =>
-            prevVisibility.map((isVisible, index) => index === id ? false : isVisible));
+        setSnowflakes(prevSnowflakes => prevSnowflakes.map(snowflake => 
+            snowflake.id === id ? { ...snowflake, isVisible: false } : snowflake
+        ));
     };
 
     const createSnowflakes = (num) => {
-        return Array.from({ length: num }, (_, index) => {
-            const left = Math.random() * 100;
-            const animationDuration = 5 + Math.random() * 10;
-            const animationDelay = Math.random() * 5;
-
-            return (
-                <Snowflake
-                    key={index}
-                    id={index}
-                    left={left}
-                    duration={animationDuration}
-                    delay={animationDelay}
-                    isVisible={snowflakesVisibility[index]}
-                    onClick={handleSnowflakeClick}
-                />
-            );
+        return Array.from({ length: num }, (_, id) => {
+            return {
+                id,
+                left: Math.random() * 100,
+                duration: 5 + Math.random() * 10,
+                delay: Math.random() * 5,
+                isVisible: true
+            };
         });
     };
 
@@ -43,7 +34,15 @@ const CountdownTimer = () => {
             <h1>❄️KBS Winter Break!❄️</h1>
             <p>Click on the snowflakes to increase your score!</p>
             <div>Score: {score}</div>
-            {snowflakes}
+            {snowflakes.map(snowflake => 
+                snowflake.isVisible && (
+                    <Snowflake
+                        key={snowflake.id}
+                        {...snowflake}
+                        onClick={() => handleSnowflakeClick(snowflake.id)}
+                    />
+                )
+            )}
         </div>
     );
 };
--- src/components/Snowflake.js
+++ src/components/Snowflake.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
-const Snowflake = ({ id, left, duration, delay, onClick, isVisible }) => {
+const Snowflake = ({ id, left, duration, delay, onClick }) => {
     const style = {
         position: 'absolute',
         left: `${left}%`,
-        top: '-5%', // Start above the viewport
+        top: '-5%',
         animationDuration: `${duration}s`,
         animationDelay: `${delay}s`,
-        cursor: 'pointer', // Change cursor to indicate clickable
-        display: isVisible ? 'block' : 'none', // Control visibility
+        cursor: 'pointer'
     };
 
-    return <div className="snowflake" style={style} onClick={() => onClick(id)}>❄️</div>;
+    return <div className="snowflake" style={style} onClick={onClick}>❄️</div>;
 };
 
 export default Snowflake;
